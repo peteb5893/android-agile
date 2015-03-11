a1c6b622de47fb38f8e7c01aea18cde78d4139dd
kaspars-greizis <kaspars.greizis+git@gmail.com>
2015-03-11 17:04:46 +0000

    puzzle. java additional test code

--- AndroidDashboardDesign/src/com/aptitudeguru/dashboard/Puzzles.java
+++ AndroidDashboardDesign/src/com/aptitudeguru/dashboard/Puzzles.java
@@ -1,21 +1,28 @@
 package com.aptitudeguru.dashboard;
 
 import java.util.Locale;
+import java.util.Currency;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidhive.dashboard.R;
 
+
 public class Puzzles extends Activity {
 	TextView mainTextView;
 	Locale locale = Locale.getDefault();
-	
+	Currency curr = Currency.getInstance(locale);
+	String $curr=curr.getSymbol(locale);
+	String $dist;
+	String $speed;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.puzzles);
 		
 		mainTextView = (TextView) findViewById(R.id.mainTextView);		
-		mainTextView.setText("Current locale: " + locale.toString());
+		
+		mainTextView.setText("Current locale: " + locale.toString() + "\nCurrency symbol: " + $curr +"\nWithin a string: $dist $curr test");
+		//mainTextView.setText("$dist $curr whatever else, test");
 	}
 }
