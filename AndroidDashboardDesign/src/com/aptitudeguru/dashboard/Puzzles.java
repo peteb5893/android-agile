ab5b5b8dc2f8d627ccfb8327487d67a92664014c
kaspars-greizis <kaspars.greizis+git@gmail.com>
2015-03-13 15:25:34 +0000

    Merging new stuff

--- AndroidDashboardDesign/src/com/aptitudeguru/dashboard/Puzzles.java
+++ AndroidDashboardDesign/src/com/aptitudeguru/dashboard/Puzzles.java
@@ -13,20 +13,25 @@ public class Puzzles extends Activity {
 	TextView mainTextView;
 	Locale locale = Locale.getDefault();
 	Currency curr = Currency.getInstance(locale);
-	String $curr=curr.getSymbol(locale);
-	String $dist;
-	String $speed;
+	String cachemoney=curr.getSymbol(locale);
+	String distvar;
+	String speedvar;
+	String teststr = "distvar cachemoney whatever else, test";
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.puzzles);
 		
 		mainTextView = (TextView) findViewById(R.id.mainTextView);		
 		
-		mainTextView.setText("Current locale: " + locale.toString() + "\nCurrency symbol: " + $curr +"\nWithin a string: $dist $curr test");
+		//mainTextView.setText("Current locale: " + locale.toString() + "\nCurrency symbol: " + cachemoney +"\nWithin a string: $dist $curr test");
 		//mainTextView.setText("$dist $curr whatever else, test");
+		String newStr = teststr.replace("cachemoney", cachemoney);
+		System.out.println(newStr);
 		mainTextView.setText("Current Locale: " + locale.toString() +
 							"\nCountry Initials: " + locale.getCountry() + 
 							"\nCountry Name: " + locale.getDisplayCountry() +
-							"\nCountry Language: " + locale.getDisplayLanguage());
+							"\nCountry Language: " + locale.getDisplayLanguage() + 
+							"\nCurrency: " + cachemoney + 
+							"\nReplace test: " + newStr);
 	}
 }
