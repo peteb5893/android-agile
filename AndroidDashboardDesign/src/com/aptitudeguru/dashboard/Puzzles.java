44ce113771741bb021f3a7787b800489528c784d
txroberts <t.x.roberts@dundee.ac.uk>
2015-03-20 17:07:50 +0000

    Stopped duplicate input
    Check if the user has entered the same answer twice

--- AndroidDashboardDesign/src/com/aptitudeguru/dashboard/Puzzles.java
+++ AndroidDashboardDesign/src/com/aptitudeguru/dashboard/Puzzles.java
@@ -9,21 +9,35 @@ import android.view.View;
 import android.view.View.OnFocusChangeListener;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidhive.dashboard.R;
 
 public class Puzzles extends Activity{
 	EditText editText1, editText2;
 	TextView textView1;
 
+	// validate that the user typed in A, B, C, or D
 	public boolean validInput(String input) {
-		input = input.toLowerCase(Locale.getDefault());
-		if (input.equals("a") || input.equals("b") || input.equals("c") || input.equals("d"))
+		input = input.toLowerCase(Locale.getDefault()); // normalise input to lower case
+		if (input.equals("a") || input.equals("b") || input.equals("c") || input.equals("d")){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	// check that the user hasn't entered the same answer twice
+	public boolean isDuplicateInput(EditText editTextA, EditText editTextB){
+		String input1 = editTextA.getText().toString();
+		String input2 = editTextB.getText().toString();
+		
+		if (input1.equals(input2)) // compare the text typed into both text entry boxes
 			return true;
 		else
 			return false;
 	}
 	
+	// listener for when an edit text box loses focus (the user taps away from it)
 	private void setOnFocusChangeListener(final EditText editText){
 		editText.setOnFocusChangeListener(new OnFocusChangeListener() {
 
@@ -32,8 +46,15 @@ public class Puzzles extends Activity{
 				if(!hasFocus){
 					String input = editText.getText().toString();
 					
-					if(validInput(input)) {
-						textView1.setText(input);
+					editText.setError(null); // clear any previous errors
+					
+					if(validInput(input)) { // user entered a valid input
+						if (!isDuplicateInput(editText1, editText2)){ // user didn't enter a duplicate input
+							textView1.setText(input);
+						}
+						else {
+							editText.setError("Already entered " + input + ", select another option");
+						}
 					}
 					else {
 						editText.setError("Enter A, B, C, D");
